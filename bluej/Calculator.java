ef864ecee52e704fe344deb8d9335059c52c444f
MarkusSprunck <markus.sprunck@mnet-online.de>
2023-02-07 10:01:25 +0100

    Add BlueJ example

---
+++ bluej/Calculator.java
@@ -0,0 +1,62 @@
+
+import java.util.Locale;
+
+public class Calculator {
+
+    public static final String INVALID_INPUT = "INVALID_INPUT";
+
+    public static String run(String calculation) {
+        DataModel dataModel = parse(calculation);
+        if (dataModel.isValid()) {
+            return calculate(dataModel);
+        }
+        return INVALID_INPUT;
+    }
+
+    private static String calculate(DataModel dataModel) {
+        String result = INVALID_INPUT;
+        switch (dataModel.getOperator()) {
+            case ADD -> result = addition(dataModel);
+            case SUB -> result = subtraction(dataModel);
+            case MULT -> result = multiplication(dataModel);
+            case DIV -> result = division(dataModel);
+        }
+        return result;
+    }
+
+    private static DataModel parse(String calculation) {
+        DataModel dataModel = new DataModel();
+        if (calculation != null) {
+            String[] values = calculation.trim().replace("  ", " ").split(" ");
+            if (values.length == 3) {
+                try {
+                    dataModel.setOperator(Operators.getFromString(values[1]));
+                    dataModel.setNumberOne(values[0]);
+                    dataModel.setNumberTwo(values[2]);
+                } catch (IllegalArgumentException ignored) {
+                    // nothing to do
+                }
+            }
+        }
+        return dataModel;
+    }
+
+    private static String addition(DataModel dataModel) {
+        return "" + (dataModel.getNumberOne() + dataModel.getNumberTwo());
+    }
+
+    private static String subtraction(DataModel dataModel) {
+        return "" + (dataModel.getNumberOne() - dataModel.getNumberTwo());
+    }
+
+    private static String multiplication(DataModel dataModel) {
+        return "" + (dataModel.getNumberOne() * dataModel.getNumberTwo());
+    }
+
+    private static String division(DataModel dataModel) {
+        if (dataModel.getNumberTwo() == 0) {
+            return "ERROR";
+        }
+        return String.format(Locale.UK,"%.3f", (double) dataModel.getNumberOne() / dataModel.getNumberTwo());
+    }
+}
---
+++ bluej/DataModel.java
@@ -0,0 +1,32 @@
+
+public class DataModel {
+    private Operators operator;
+    private Long numberOne = null;
+    private Long numberTwo = null;
+    public boolean isValid() {
+        return this.numberOne != null && this.numberTwo != null && !this.operator.equals(Operators.DEFAULT);
+    }
+    public void setNumberOne(String value) throws NumberFormatException {
+        this.numberOne = Long.valueOf(Integer.parseInt(value));
+    }
+    public void setNumberTwo(String value) throws NumberFormatException {
+        this.numberTwo = Long.valueOf(Integer.parseInt(value));
+    }
+
+    public Operators getOperator() {
+        return this.operator;
+    }
+
+    public Long getNumberOne() {
+        return this.numberOne;
+    }
+
+    public Long getNumberTwo() {
+        return this.numberTwo;
+    }
+
+    public void setOperator(Operators operator) {
+        this.operator = operator;
+    }
+
+}
